0607eda039a3f8fa8aa3f99670528edd74240496
Konstantin <atlant1995@yandex.ru>
2018-07-08 23:15:01 +0700

    header fixed

--- dist/js/main.js
+++ dist/js/main.js
@@ -6,6 +6,27 @@ var scroll_left;
 
 
 $(document).ready(function(){
+	var menu = $('.header__bottom');
+	var menuHeight = $('.header__bottom').height();
+	var menuPosition = $('.header__bottom').offset().top;
+	function menuFixed(){
 
-	
+		var windowPosition = $(window).scrollTop();
+		if (windowPosition >= menuPosition){
+			menu.css("position", "fixed");
+			menu.css("top", "0px");
+			console.log('fixed')
+
+		} else {
+			menu.css("position","absolute");
+			menu.css("top", menuPosition + 'px');
+			console.log('abs')
+
+		}
+		console.log('1', menuPosition, menuHeight)
+	};
+	$(window).scroll(function() {
+		menuFixed()
+
+	})
 })
