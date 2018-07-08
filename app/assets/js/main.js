f7c21cd3904aa1f051db2f68476762c28701fcf8
Konstantin <atlant1995@yandex.ru>
2018-07-08 23:34:02 +0700

    last correction

--- app/assets/js/main.js
+++ app/assets/js/main.js
@@ -6,6 +6,24 @@ var scroll_left;
 
 
 $(document).ready(function(){
+	var menu = $('.header__bottom');
+	var menuHeight = $('.header__bottom').height();
+	var menuPosition = $('.header__bottom').offset().top;
+	function menuFixed(){
 
-	
+		var windowPosition = $(window).scrollTop();
+		if (windowPosition >= menuPosition){
+			menu.css("position", "fixed");
+			menu.css("top", "0px");
+
+		} else {
+			menu.css("position","absolute");
+			menu.css("top", menuPosition + 'px');
+
+		}
+	};
+	$(window).scroll(function() {
+		menuFixed()
+
+	})
 })
